8d5460538d6e42800ed854b3dc8c880dec2557bb
Mike Nguyen <peps.minhnguyen@gmail.com>
2019-09-24 20:46:45 +0000

    edit timer

--- timer.js
+++ timer.js
@@ -1,9 +1,12 @@
 let argsInput = process.argv.slice(2);
 let num = argsInput.map(each => Number(each));
 
-for (let i = 0; i < argsInput.length; i++) {
-  setTimeout(() => {
-    process.stdout.write('\x07');
-  }, argsInput[i]*1000)
+for (let i = 0; i < num.length; i++) {
+  if (num[i] > 0 && (typeof num[i]) === 'number'){
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, argsInput[i]*1000);
+  } else (
+    null
+  )
 }
-
