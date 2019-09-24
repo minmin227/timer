eede097e34e57ef100ae39c563d9bd66bc6d5261
Mike Nguyen <peps.minhnguyen@gmail.com>
2019-09-24 20:26:12 +0000

    add timer

--- timer.js
+++ timer.js
@@ -1,1 +1,9 @@
-process.stdout.write('\x07');
+let argsInput = process.argv.slice(2);
+let num = argsInput.map(each => Number(each));
+
+for (let i = 0; i < argsInput.length; i++) {
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, argsInput[i]*1000)
+}
+
